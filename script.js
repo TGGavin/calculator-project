04de203fb2e0253789ba3739e2346ede153a7146
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-03-19 15:56:10 -0700

    Add divide function, add multiply function

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function addition(num, num2) {
 function subtract(num, num2) {
     return num - num2
 }
+
+function multiply(num, num2) {
+    return num * num2
+}
+
+function divide(num, num2) {
+    return num / num2
+}
