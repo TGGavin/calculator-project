59df70281eca255cf9c655a2a2d8caeda25e0918
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-03-19 19:10:52 -0700

    Add screenValues object, with placeholder vals. edit operation function to change screenValues object propeties, and use operators object with parameters
    operation() changes screenValues for screen yet to be made, function uses parameters for operators object methods

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 
-    const operators = {
-        add: (a, b) => a + b,
-        minus: (a, b) => a - b,
-        multiply: (a, b) => a * b,
-        divide: (a, b) => a / b
-    }
+let screenValues = {
+    operator: "No Operator",
+    operand1: "No Number1",
+    operand2: "No Number2"
+}
 
-    function operation(num, operator, num2) {
-        
-    }
+const operators = {
+    add: (a, b) => a + b,
+    minus: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+}
+
+function operation(operator, num, num2) {
+    screenValues["operator"] = operator
+    screenValues["operand1"] = num
+    screenValues["operand2"] = num2
+    return operators[operator](num, num2)
+}
