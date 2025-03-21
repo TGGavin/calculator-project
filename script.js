6217da20cf745bac0c0326f310c180c5e330c72a
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-03-21 16:32:46 -0700

    Add all selected calculator buttons, comment currently opsolete code
    Need selected constants for future event handlers

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-// used to update dislay, (later)
-let screenValues = {
-    operator: "No Operator",
-    operand1: "No Number1",
-    operand2: "No Number2"
+// used to update display, (later)
+// let screenValues = {
+//     operator: "No Operator",
+//     operand1: "No Number1",
+//     operand2: "No Number2"
+// }
+
+const zeroBtn = document.querySelector(".zero-btn")
+const oneBtn = document.querySelector(".one-btn")
+const twoBtn = document.querySelector(".two-btn")
+const threeBtn = document.querySelector(".three.btn")
+const fourBtn = document.querySelector(".four-btn")
+const fiveBtn = document.querySelector(".five-btn")
+const sixBtn = document.querySelector(".six-btn")
+const svnBtn = document.querySelector(".svn-btn")
+const eightBtn = document.querySelector(".eight-btn")
+const nineBtn = document.querySelector(".nine-btn")
+const negateBtn = document.querySelector(".negate-btn")
+const decimalBtn = document.querySelector(".decimal-btn")
+const delBtn = document.querySelector(".del-btn")
+const divideBtn = document.querySelector(".divide-btn")
+const multiplyBtn = document.querySelector(".multiply-btn")
+const subtractBtn = document.querySelector(".subtract-btn")
+const addBtn = document.querySelector(".add-btn")
+const clearBtn = document.querySelector(".clear-btn")
+const equalsBtn = document.querySelector(".equal-btn")
+
+
+const calScreen = document.querySelector("#calc-screen")
+function disUpdate(num) {
+    return calScreen.textContent = num
 }
 
 const operators = {
@@ -13,8 +39,8 @@ const operators = {
 }
 
 function operation(operator, num, num2) {
-    screenValues["operator"] = operator
-    screenValues["operand1"] = num
-    screenValues["operand2"] = num2
+    // screenValues["operator"] = operator
+    // screenValues["operand1"] = num
+    // screenValues["operand2"] = num2
     return operators[operator](num, num2)
 }
