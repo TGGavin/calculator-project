19296322c6cd41cefbdea190c802d53e730c1d0c
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-03-24 15:19:30 -0700

    Add tOrB() to determine change first or second operand value, add unfinished cNumProcess() that used tOrB(), remove zeroArbiter()

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 //  TO DO LIST 
 //  Make upper-cal-num disappear when not in use
 //  Make functioning calculator before visuals
+//  Make boolean value determining whther to edit upper or lower value, if operator is selected
 
 
-let calNum1 = 0
-let calOp;
-let calNum2 = 0
-// let mainOpStr = 0
-// let mainOpVal = parseInt(mainOpStr)
+let calNum1 = "0";
+let calOp = "";
+let calNum2 = "0";
 
-const lowerCal = document.querySelector(".lower-cal-num")
+const calScreen = document.querySelector(".cal-screen");
+const upperCal = document.querySelector(".cal-upper-content");
+const lowerCal = document.querySelector(".lower-cal-num");
 
-const calDis = () => lowerCal.textContent = calNum1
-function zeroArbiter(strungNum) {
-    let startsWithZero = strungNum.startsWith("0")
-    if (strungNum.length > 1 && startsWithZero) {
-        return strungNum.replace("0", "") 
-    } else {
-        return strungNum
-    }
+// Calculator value updater for html spans, so accurate values are displayed
+function textContentUpdate(elem, val) {
+    elem.textContent = val;
 }
 
-// will make spans appear and disappear later
-// function TBD() {
-    // const screen = document.querySelector(".cal-screen")
-    // const upper = document.querySelector(".cal-upper-content")
-    // screen.removeChild(upper)
-    // screen.appendChild(upper)
+// function calSpanUp() {
+//     textContentUpdate()
 // }
+
+// Determines to change top or bottom calculator value
+function tOrB() {
+    let hasOps = (opVal) => {
+        switch (opVal) {
+            case "-":
+            case "+":
+            case "/":
+            case "*":
+                return true;
+            case "":
+                return false;
+            default:
+                return 404;
+            }
+        }
+        let trueOrFalse = hasOps(calOp)
+        return trueOrFalse
+}
+
+function cNumProcess(str) {
+    let parsedStr = parseInt(str)
+    // if (isTopOrBottom) {
+        textContentUpdate(upperCal, parsedStr)
+    // } else if (isTopOrBottom) {
+    //     textContentUpdate(lowerCal, parsedStr)
+    // }
+}
 
 const operators = {
     add: (a, b) => a + b,
@@ -41,31 +61,30 @@ function operation(operator, num, num2) {
     return operators[operator](num, num2)
 }
 
-
 // List of all calculator buttons
-const zeroBtn = document.querySelector(".zero-btn")
-const oneBtn = document.querySelector(".one-btn")
-const twoBtn = document.querySelector(".two-btn")
-const threeBtn = document.querySelector(".three-btn")
-const fourBtn = document.querySelector(".four-btn")
-const fiveBtn = document.querySelector(".five-btn")
-const sixBtn = document.querySelector(".six-btn")
-const svnBtn = document.querySelector(".svn-btn")
-const eightBtn = document.querySelector(".eight-btn")
-const nineBtn = document.querySelector(".nine-btn")
-const negateBtn = document.querySelector(".negate-btn")
-const decimalBtn = document.querySelector(".decimal-btn")
-const delBtn = document.querySelector(".del-btn")
-const divideBtn = document.querySelector(".divide-btn")
-const multiplyBtn = document.querySelector(".multiply-btn")
-const subtractBtn = document.querySelector(".subtract-btn")
-const addBtn = document.querySelector(".add-btn")
-const clearBtn = document.querySelector(".clear-btn")
-const equalsBtn = document.querySelector(".equal-btn")
+const zeroBtn = document.querySelector(".zero-btn");
+const oneBtn = document.querySelector(".one-btn");
+const twoBtn = document.querySelector(".two-btn");
+const threeBtn = document.querySelector(".three-btn");
+const fourBtn = document.querySelector(".four-btn");
+const fiveBtn = document.querySelector(".five-btn");
+const sixBtn = document.querySelector(".six-btn");
+const svnBtn = document.querySelector(".svn-btn");
+const eightBtn = document.querySelector(".eight-btn");
+const nineBtn = document.querySelector(".nine-btn");
+const negateBtn = document.querySelector(".negate-btn");
+const decimalBtn = document.querySelector(".decimal-btn");
+const delBtn = document.querySelector(".del-btn");
+const divideBtn = document.querySelector(".divide-btn");
+const multiplyBtn = document.querySelector(".multiply-btn");
+const subtractBtn = document.querySelector(".subtract-btn");
+const addBtn = document.querySelector(".add-btn");
+const clearBtn = document.querySelector(".clear-btn");
+const equalsBtn = document.querySelector(".equal-btn");
 
-const calcBtnContainer = document.querySelector(".calc-btn-container")
+const calcBtnContainer = document.querySelector(".calc-btn-container");
 calcBtnContainer.addEventListener("click", (e) => {
-    target = e.target
+    target = e.target;
     switch (target) {
         case zeroBtn:
             
@@ -123,12 +142,14 @@ calcBtnContainer.addEventListener("click", (e) => {
             console.log(target);
             break;
         case clearBtn:
-            console.log(target);
+            calNum1 = 0;
+            calOp = "";
+            calNum2 = 0;
             break;
         case equalsBtn:
             console.log(target);
             break;
         // default:
-    }
+    };
 
-})
+});
